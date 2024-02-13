4800ddd463b620acacd04141a1e606c3df6c035f
neojarvis <techadmin@iamneo.ai>
2024-02-13 10:13:15 +0000

    Auto-save-commit-13/02/2024--15:43:15

--- Movie Review copy/dotnetapp/dotnetapp/Models/ApplicationDbContext.cs
+++ Movie Review copy/dotnetapp/dotnetapp/Models/ApplicationDbContext.cs
@@ -8,7 +8,27 @@ namespace dotnetapp.Models
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
-        public DbSet<Cricket> Crickets { get; set; }
+        public DbSet<CricketTournament> CricketTournaments { get; set; }
       
     }
 }
+
+
+
+//  dotnet new tool-manifest
+
+ 
+
+// dotnet tool install --local dotnet-ef --version 6.0.6
+
+ 
+
+// dotnet dotnet-ef --To check the EF installed or not
+
+
+
+// dotnet dotnet-ef migrations add "InitialSetup" --command to setup the initial creation of tables mentioned in DBContext
+
+ 
+
+// dotnet dotnet-ef database update --command to update the database

4bf7f5a286e468fef11083a202bcdce15099d613
neojarvis <techadmin@iamneo.ai>
2024-02-13 11:59:05 +0000

    Auto-save-commit-13/02/2024--17:29:04

--- Movie Review copy/dotnetapp/dotnetapp/Controllers/UserController.cs
+++ Movie Review copy/dotnetapp/dotnetapp/Controllers/UserController.cs
@@ -82,15 +82,6 @@ public class AuthController : ControllerBase
     {
         try
         {
-            // var authorizationHeader = HttpContext.Request.Headers["Authorization"];
-            // var token = authorizationHeader.ToString().Replace("Bearer ", string.Empty);
-            // Console.WriteLine("Token" + token);
-            // bool a = AuthService.ValidateJwt(token);
-            // Console.WriteLine("value is " + a);
-            // if (a == false)
-            // {
-            //     return Unauthorized(new { message ="Invalid or expired token"});
-            // }          
             var users = await _context.Users.Select(u => new { u.FirstName, u.LastName, u.Role, u.UserId,u.Email,u.MobileNumber }).ToListAsync();
             return Ok(users);
         }
@@ -99,27 +90,6 @@ public class AuthController : ControllerBase
             return StatusCode(500, new { message = ex.Message });
         }
     }
-
-    //     private string GenerateToken(int userId)
-    //     {
-
-    //         var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(HardcodedJwtSecretKey));
-    //         var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-    //         var claims = new[]
-    //         {
-    //             new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
-    //             // Add additional claims if needed
-    //         };
-
-    //         var token = new JwtSecurityToken(
-    //             claims: claims,
-    //             expires: DateTime.UtcNow.AddHours(2), // Token expiry time
-    //             signingCredentials: credentials
-    //         );
-
-    //         return new JwtSecurityTokenHandler().WriteToken(token);
-    //     }
 }
 public class LoginRequestModel
 {

85d0103fd717624c1bc4c302ac7b0f22d51991d9
neojarvis <techadmin@iamneo.ai>
2024-02-13 06:58:15 +0000

    Auto-save-commit-13/02/2024--12:28:15

--- frontend/src/Tenant/ProductDetails.jsx
+++ frontend/src/Tenant/ProductDetails.jsx
@@ -8,7 +8,7 @@ const RentalList = () => {
     const [rentals, setRentals] = useState([]);
     const [selectedRental, setSelectedRental] = useState(null);
     const [showPopup, setShowPopup] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState(null);
     const [sortValue, setSortValue] = useState(1);
     const navigate = useNavigate();
 

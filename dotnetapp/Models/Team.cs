95af0cca3fdd7a74a6b3e5ec09ca8e8096afd64f
neojarvis <techadmin@iamneo.ai>
2024-02-14 07:02:12 +0000

    Auto-save-commit-14/02/2024--12:32:10

---
+++ dotnetapp/Models/Team.cs
@@ -0,0 +1,22 @@
+﻿namespace dotnetapp.Models
+{
+  
+
+public class Team
+{
+    public int TeamId { get; set; }
+
+    [Required(ErrorMessage = "Team name is required")]
+    public string TeamName { get; set; }
+
+    [Required(ErrorMessage = "Team owner is required")]
+    public string TeamOwner { get; set; }
+
+    [Range(1800, 2100, ErrorMessage = "Established year must be between 1800 and 2100")]
+    public int EstablishedYear { get; set; }
+
+    // Navigation property representing the players in the team
+    public List<Player> Players { get; set; }
+}
+
+}

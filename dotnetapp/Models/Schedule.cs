3df4052318e47238ede6afc6785f003717ed13de
neojarvis <techadmin@iamneo.ai>
2024-02-15 10:19:45 +0000

    Auto-save-commit-15/02/2024--15:49:44

--- dotnetapp/Models/Schedule.cs
+++ dotnetapp/Models/Schedule.cs
@@ -24,10 +24,14 @@ namespace dotnetapp.Models
         // [Required(ErrorMessage = "Referee is required")]
         public int RefereeId { get; set; }
 
-        public Referee MainReferee { get; set; }
+        public Referee Referee { get; set; }
 
         public int Team1Id { get; set; }
+        public Team Team1 { get; set; }
+
         public int Team2Id { get; set; }
+        public Team Team2 { get; set; }
+
 
     }
 }
